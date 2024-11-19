606ea6c44b1eea9ddd5fb603f57052806078c65e
ad-code-2425 <ad.code.2425@gmail.com>
2024-11-19 18:25:11 +0100

    ++ try-with-resources

--- src/main/java/modelo/main/ud2/OraclePersonMappingExample.java
+++ src/main/java/modelo/main/ud2/OraclePersonMappingExample.java
@@ -14,49 +14,49 @@ import oracle.jdbc.datasource.impl.OracleDataSource;
 
 public class OraclePersonMappingExample {
     public static void main(String[] args) {
-        
 
         // <!-- https://www.oracle.com/database/technologies/faq-jdbc.html -->
 
-        OracleDataSource ods = null;
-        Connection conn = null;
         try {
-            ods = new OracleDataSource();
+            OracleDataSource ods = new OracleDataSource();
 
             // Update url por la ip que corresponda
             String url = "jdbc:oracle:thin:@localhost:1521/xepdb1";
             ods.setURL(url);
             ods.setUser("people_user");
             ods.setPassword("abc123.");
-            conn = ods.getConnection();
-
-            // Registrar el mapeo del tipo UDT (user defined type)
-            ((OracleConnection) conn).setTypeMap(Map.of("PERSON_TYP", Person.class));
 
             String query = "SELECT value(p) from person_obj_table2 p ";
+            try (
+                    Connection conn = ods.getConnection();
+                    PreparedStatement stmt = conn.prepareStatement(query);) {
 
-            PreparedStatement stmt = conn.prepareStatement(query);
+                // Registrar el mapeo del tipo UDT (user defined type)
+                ((OracleConnection) conn).setTypeMap(Map.of("PERSON_TYP", Person.class));
 
-            List<Person> people = new ArrayList<>();
-            // Ejecutar la consulta
-            ResultSet rs = stmt.executeQuery();
+                List<Person> people = new ArrayList<>();
+                // Ejecutar la consulta
+                ResultSet rs = stmt.executeQuery();
 
-            while (rs.next()) {
-                Person person = (Person) rs.getObject(1);
-                people.add(person); // Add to the list
+                while (rs.next()) {
+                    Person person = (Person) rs.getObject(1);
+                    people.add(person); // Add to the list
+                }
+
+                System.out.println("Personas recuperadas:");
+                for (Person person : people) {
+                    person.displayDetails();
+                    System.out.println("-------------");
+                }
+                if (people.size() == 0) {
+                    System.out.println("No se ha recuperado ningún registro");
+                }
+
+            } catch (SQLException e) {
+                e.printStackTrace();
             }
-
-            System.out.println("Personas recuperadas:");
-            for (Person person : people) {
-                person.displayDetails();
-                System.out.println("-------------");
-            }
-            if(people.size()==0){
-                System.out.println("No se ha recuperado ningún registro");
-            }
-
-        } catch (SQLException e) {
-            e.printStackTrace();
+        } catch (SQLException ex) {
+            ex.printStackTrace();
         }
     }
 }
